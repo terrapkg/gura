f5f6395378b3a6411afce62f9db399ff6214257b
Jamie Murphy <hello@itsjamie.dev>
2024-08-26 03:15:32 -0700

    download: Append revision to name, pipe bytes through XZ decoder

--- src/database/download.rs
+++ src/database/download.rs
@@ -14,40 +14,43 @@
 // If not, see <https://www.gnu.org/licenses/>.
 
 use rocket::futures::StreamExt;
-use rocket::tokio::fs::File;
-use rocket::tokio::io::BufWriter;
+use std::fs::File;
+use std::io::BufWriter;
 
 use crate::models::Repodata;
 
-async fn get_file_url(repodata: String) -> Result<String, Box<dyn std::error::Error>> {
+async fn get_file_url(repodata: String) -> Result<(String, String), Box<dyn std::error::Error>> {
     let xml_stream = reqwest::get(repodata).await?.text().await?;
 
     let xml: Repodata = serde_xml_rs::from_str(&xml_stream).unwrap();
 
     if let Some(loc) = xml.data.iter().find(|data| data._type == "primary_db") {
         let href = loc.location.href.clone().replace("repodata/", "");
-        return Ok(href);
+        return Ok((href, xml.revision));
     }
 
     Err("File Not Found".into())
 }
 
+/// Returns the path to the downloaded database.
 pub async fn download() -> Result<String, Box<dyn std::error::Error>> {
     const DOWNLOAD_URL: &str = "https://repos.fyralabs.com/terra41/repodata/";
 
-    let file_url = get_file_url(DOWNLOAD_URL.to_owned() + "/repomd.xml").await?;
+    let (file_url, revision) = get_file_url(DOWNLOAD_URL.to_owned() + "/repomd.xml").await?;
 
     let mut stream = reqwest::get(DOWNLOAD_URL.to_owned() + &file_url)
         .await?
         .bytes_stream();
 
     let mut filename = std::env::temp_dir();
-    filename.push("DATABASE.sqlite.xz");
+    filename.push(format!("primary-{}.sqlite", revision));
 
-    let mut out = BufWriter::new(File::create(filename.clone()).await?);
+    let out = BufWriter::new(File::create(filename.clone())?);
+
+    let mut xz_decode = xz::write::XzDecoder::new(out);
 
     while let Some(chunk) = stream.next().await {
-        rocket::tokio::io::copy(&mut chunk?.as_ref(), &mut out).await?;
+        std::io::copy(&mut chunk?.as_ref(), &mut xz_decode)?;
     }
 
     filename
