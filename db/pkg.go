03dde17a353af458ed5d01a5028fd7c9f35f7e13
Cappy Ishihara <cappy@cappuchino.xyz>
2025-09-09 21:59:33 +0700

    Add PkgFetchMulti and PkgQuerySQL helpers to pkg DB layer

--- db/pkg.go
+++ db/pkg.go
@@ -20,18 +20,35 @@ func PkgFetch(pkg_id uuid.UUID) (*Pkg, error) {
 	return &pkg, nil
 }
 
+func PkgFetchMulti(pkg_ids []uuid.UUID) ([]Pkg, error) {
+	var pkgs []Pkg
+	if err := DB.Where("id IN ?", pkg_ids).Find(&pkgs).Error; err != nil {
+		return nil, err
+	}
+	return pkgs, nil
+}
+
+// GORM helper for querying packages with arbitrary conditions
+func PkgQuerySQL(query interface{}, args ...interface{}) ([]Pkg, error) {
+	var pkgs []Pkg
+	if err := DB.Where(query, args...).Find(&pkgs).Error; err != nil {
+		return nil, err
+	}
+	return pkgs, nil
+}
+
 func SubmitPkg(pkg *Pkg) (*Pkg, error) {
-    if err := DB.Create(pkg).Error; err != nil {
-        return nil, err
-    }
-    return pkg, nil
+	if err := DB.Create(pkg).Error; err != nil {
+		return nil, err
+	}
+	return pkg, nil
 }
 
 func (p *Pkg) Update() error {
-    if err := DB.Save(p).Error; err != nil {
-        return err
-    }
-    return nil
+	if err := DB.Save(p).Error; err != nil {
+		return err
+	}
+	return nil
 }
 
 func (p *Pkg) Delete() error {
